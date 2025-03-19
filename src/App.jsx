31e5b42a2333eb64fab314e9dedcda86dddf14ba
AlexDegerman <alex.degerman.dev@gmail.com>
2025-03-19 19:28:04 +0200

    Add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import Header from './components/Header'
 
 function App() {
 
 
   return (
-    <div></div>
+    <div className="app-container">
+      <Header/>
+    </div>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import "../styles/Header.css";
+
+const Header = () => {
+  
+  return (
+    <div className="header">
+      <h1>eCommerceApp</h1>
+      <h2>Products</h2>
+      <h2>Cart</h2>
+    </div>
+  )
+}
+
+export default Header
