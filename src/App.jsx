8ce75f1d3b138ee0736c56243a7c2a3d92160ab6
AlexDegerman <alex.degerman.dev@gmail.com>
2025-03-20 02:13:48 +0200

    Add routing and product list

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Header from './components/Header'
+import ProductList from './components/ProductList'
 
-function App() {
+const App = () => {
 
 
   return (
     <div className="app-container">
       <Header/>
+      <Routes>
+        <Route path="/product-list" element={<ProductList/>}/>
+      </Routes>
     </div>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
+import { Link } from "react-router-dom";
 import "../styles/Header.css";
 
 const Header = () => {
   
   return (
     <div className="header">
-      <h1>eCommerceApp</h1>
-      <h2>Products</h2>
+      <Link to="/"><h1>eCommerceApp</h1></Link>
+      <Link to="/product-list"><h2>Products</h2></Link>
       <h2>Cart</h2>
     </div>
   )
